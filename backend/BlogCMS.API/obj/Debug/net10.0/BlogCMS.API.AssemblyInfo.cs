5362997e26ed83b48cca1c37080f61d734c7f03e
Renuka Chikkam <chikkamrenukachikkam@gmail.com>
2026-09-09 12:18:46 +0530

    Added project screenshots

--- backend/BlogCMS.API/obj/Debug/net10.0/BlogCMS.API.AssemblyInfo.cs
+++ backend/BlogCMS.API/obj/Debug/net10.0/BlogCMS.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogCMS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+232b4db39f0564fd81016870e9fbdf8c9bf9d6fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogCMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogCMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
